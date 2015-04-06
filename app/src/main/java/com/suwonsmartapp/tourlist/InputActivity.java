599c2cd50dde1f96255bb18ff8b67cb002bcabb3
Junsuk Oh <a811219@gmail.com>
2015-04-06 11:32:30 +0900

    Activity 연결

--- app/src/main/java/com/suwonsmartapp/tourlist/InputActivity.java
+++ app/src/main/java/com/suwonsmartapp/tourlist/InputActivity.java
@@ -1,11 +1,13 @@
 
 package com.suwonsmartapp.tourlist;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.EditText;
 
-public class InputActivity extends ActionBarActivity {
+public class InputActivity extends ActionBarActivity implements View.OnClickListener {
 
     private EditText mtitleFront;
     private EditText mtitleMiddle;
@@ -22,7 +24,12 @@ public class InputActivity extends ActionBarActivity {
         mtitleRear = (EditText) findViewById(R.id.title_rear);
         mcontents = (EditText) findViewById(R.id.contents);
 
+        findViewById(R.id.submit_Btn).setOnClickListener(this);
     }
 
 
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(getApplicationContext(), ResultsActivity.class));
+    }
 }
--- app/src/main/java/com/suwonsmartapp/tourlist/list/ListActivity.java
+++ app/src/main/java/com/suwonsmartapp/tourlist/list/ListActivity.java
@@ -1,14 +1,18 @@
 package com.suwonsmartapp.tourlist.list;
 
+import com.suwonsmartapp.tourlist.InputActivity;
+import com.suwonsmartapp.tourlist.R;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
-
-import com.suwonsmartapp.tourlist.R;
 
 import java.util.ArrayList;
 
-public class ListActivity extends ActionBarActivity {
+public class ListActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
     public static final String TAG = ListActivity.class.getSimpleName();
     private ListView mListAdapter;
     private ArrayList<TourList> tourList;
@@ -28,6 +32,8 @@ public class ListActivity extends ActionBarActivity {
 
         // View에 붙이기
         mListAdapter.setAdapter(adapter);
+
+        mListAdapter.setOnItemClickListener(this);
     }
 
     void tourListLoad(){
@@ -42,5 +48,10 @@ public class ListActivity extends ActionBarActivity {
         tourList.add(new TourList(R.drawable.girl, "제목 제목 8", "2015-08-01", "본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용  본문내용", R.drawable.car, R.drawable.girl, R.drawable.gold_apple, R.drawable.car, R.drawable.car));
         tourList.add(new TourList(R.drawable.gold_apple, "제목 제목 9", "2015-09-01", "본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용  본문내용", R.drawable.car, R.drawable.girl, R.drawable.gold_apple, R.drawable.car, R.drawable.car));
         tourList.add(new TourList(R.drawable.car, "제목 제목 10", "2015-10-01", "본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용 본문내용  본문내용", R.drawable.car, R.drawable.girl, R.drawable.gold_apple, R.drawable.car, R.drawable.car));
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        startActivity(new Intent(getApplicationContext(), InputActivity.class));
     }
 }
