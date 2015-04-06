3c20bbf8676b94312dc9ca7e6c4fdba8527b33ee
nam5 <sssn000@hamail.net>
2015-04-06 17:03:36 +0900

    첫번째화면,세번째화면 메뉴추가

--- app/src/main/java/com/suwonsmartapp/tourlist/list/ListActivity.java
+++ app/src/main/java/com/suwonsmartapp/tourlist/list/ListActivity.java
@@ -1,16 +1,18 @@
 package com.suwonsmartapp.tourlist.list;
 
-import com.suwonsmartapp.tourlist.InputActivity;
-import com.suwonsmartapp.tourlist.R;
+import java.util.ArrayList;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
 
-import java.util.ArrayList;
+import com.suwonsmartapp.tourlist.InputActivity;
+import com.suwonsmartapp.tourlist.R;
 
 public class ListActivity extends ActionBarActivity implements AdapterView.OnItemClickListener {
     public static final String TAG = ListActivity.class.getSimpleName();
@@ -53,5 +55,26 @@ public class ListActivity extends ActionBarActivity implements AdapterView.OnIte
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
         startActivity(new Intent(getApplicationContext(), InputActivity.class));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_first, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        // int id = item.getItemId();
+
+        Intent intent = new Intent(this, InputActivity.class);
+        startActivity(intent);
+
+        return true;
+
     }
 }
