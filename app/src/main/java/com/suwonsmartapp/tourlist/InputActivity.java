6189295d8ef75b811f5f74c1fdb373cf14265318
juntaekryoo <juntaekryoo@gmail.com>
2015-04-07 14:17:42 +0900

    날자입력을 추가하였습니다.

--- app/src/main/java/com/suwonsmartapp/tourlist/InputActivity.java
+++ app/src/main/java/com/suwonsmartapp/tourlist/InputActivity.java
@@ -1,10 +1,17 @@
 
 package com.suwonsmartapp.tourlist;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 public class InputActivity extends ActionBarActivity implements View.OnClickListener {
@@ -13,6 +20,24 @@ public class InputActivity extends ActionBarActivity implements View.OnClickList
     private EditText mtitleMiddle;
     private EditText mtitleRear;
     private EditText mcontents;
+    private Button mTravelDateBtn;
+    private DatePicker view;
+
+    private int year;
+    private int month;
+    private int day;
+    private DatePickerDialog.OnDateSetListener dateSetListener =
+            new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                    SimpleDateFormat sf = new SimpleDateFormat("yyyy년 MM월 dd일");
+                    GregorianCalendar tmpCalendar = new GregorianCalendar();
+                    tmpCalendar.set(year, monthOfYear, dayOfMonth);
+
+                    mTravelDateBtn.setText(sf.format(tmpCalendar.getTime()));
+
+                }
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +48,25 @@ public class InputActivity extends ActionBarActivity implements View.OnClickList
         mtitleMiddle = (EditText) findViewById(R.id.title_middle);
         mtitleRear = (EditText) findViewById(R.id.title_rear);
         mcontents = (EditText) findViewById(R.id.contents);
+        mTravelDateBtn = (Button) findViewById(R.id.date_Btn);
+
+        GregorianCalendar calendar = new GregorianCalendar();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_MONTH);
+        mTravelDateBtn.setText(year + "년  " + (month + 1) + "월  " + day + "일");
+
+        mTravelDateBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog date = new DatePickerDialog(InputActivity.this, dateSetListener,
+                        year, month, day);
+                date.show();
+            }
+        });
 
         findViewById(R.id.submit_Btn).setOnClickListener(this);
     }
-
 
     @Override
     public void onClick(View v) {
