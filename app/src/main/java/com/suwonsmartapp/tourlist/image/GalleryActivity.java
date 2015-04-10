f690f4c63f5e7cc262fd859504c9d690048266e2
baeggot <ppocmila@gmail.com>
2015-04-10 13:56:21 +0900

    코드 수정

--- app/src/main/java/com/suwonsmartapp/tourlist/image/GalleryActivity.java
+++ app/src/main/java/com/suwonsmartapp/tourlist/image/GalleryActivity.java
@@ -14,11 +14,11 @@ import android.widget.GridView;
 import com.suwonsmartapp.tourlist.R;
 import com.suwonsmartapp.tourlist.image.adapter.GridAdapter;
 import com.suwonsmartapp.tourlist.image.bitmapUtil.BitmapScaleSetting;
-import com.suwonsmartapp.tourlist.image.bitmapUtil.Constant;
 import com.suwonsmartapp.tourlist.image.util.GetMaxTextureSize;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class GalleryActivity extends ActionBarActivity implements View.OnClickListener {
 
@@ -38,7 +38,6 @@ public class GalleryActivity extends ActionBarActivity implements View.OnClickLi
     // Adapter
     private GridAdapter mGridAdapter;
 
-
     private void init() {
         mBtnGetImg = (Button) findViewById(R.id.btn_get_img);
         mGridView = (GridView) findViewById(R.id.gv_each_image);
@@ -57,8 +56,7 @@ public class GalleryActivity extends ActionBarActivity implements View.OnClickLi
 
         // get a phone's max texture size
         GetMaxTextureSize getMaxTextureSize = new GetMaxTextureSize();
-        Constant.setMaxTextureSize(getMaxTextureSize.getMaxTextureSize());
-        mBitmapScaleSetting = new BitmapScaleSetting(getPackageName(), Constant.getMaxTextureSize(), getApplicationContext());
+        mBitmapScaleSetting = new BitmapScaleSetting(getPackageName(), getMaxTextureSize.getMaxTextureSize(), getApplicationContext());
 
 
         /*
@@ -105,59 +103,20 @@ public class GalleryActivity extends ActionBarActivity implements View.OnClickLi
 
         if (requestCode == SELECT_FROM_GALLERY) {
             if (resultCode == Activity.RESULT_OK) {
+                //Log.d(TAG, "image data : " + data.toString());
+
                 Uri uri = data.getData(); // Received Data from the intent
+                //Log.d(TAG, "getPath() : " + uri.getPath());
+                //Log.d(TAG, "uri.getEncodedPath() : " + uri.getEncodedPath());
 
                 mUriList.add(uri);
                 mBitmapScaleSetting.setTempImageFile();
 
                 mGridAdapter.setmShowBtns(false);
+                mGridAdapter.notifyDataSetChanged();
             }
         }
     }
 
-
-
-    /*
-        private void addBitmapToImgList(String path) {
-            // sample size 를 적용하여 bitmap load.
-            Bitmap bitmap = mBitmapScaleSetting.loadImageWithSampleSize();
-            // Bitmap bitmap =BitmapSetting.decodeSampledBitmapFromFile(getTempImageFile(), 100, 100);
-            // Bitmap bitmap =BitmapSetting.decodeSampledBitmapFromFile(getTempImageFile(), mImageSizeBoundary);
-
-            // image boundary size 에 맞도록 이미지 축소.
-            bitmap = mBitmapScaleSetting.resizeImageWithinBoundary(bitmap);
-
-            // 결과 file 을 얻어갈 수 있는 메서드 제공.
-            // saveBitmapToFile(bitmap);
-
-            // show image on ImageView (저장한 파일 읽어와서 출력하는 듯)
-            // Bitmap bm = BitmapFactory.decodeFile(getTempImageFile().getAbsolutePath());
-            // mImageView.setImageBitmap(bitmap);
-
-            TourImage tourImage = new TourImage(path, bitmap);
-            mImgList.add(tourImage);
-        }
-
-    public void addBitmapToImgListUsingCache(String path) {
-
-        // 이미지 캐시 사용 부분
-        Bitmap bitmapInCache = mImageCache.getBitmap(path);
-
-        if (bitmapInCache == null) {
-            Bitmap loadedBitmap = mBitmapScaleSetting.loadImageWithSampleSize();
-            loadedBitmap = mBitmapScaleSetting.resizeImageWithinBoundary(loadedBitmap);
-            bitmapInCache = addBitmapToCache(path, loadedBitmap);
-        }
-
-        TourImage tourImage = new TourImage(path, bitmapInCache);
-        mImgList.add(tourImage);
-    }
-
-    private Bitmap addBitmapToCache(String path, Bitmap bitmap) {
-        mImageCache.addBitmap(path, bitmap);
-        return mImageCache.getBitmap(path);
-    }
-
-    */
 
 }
